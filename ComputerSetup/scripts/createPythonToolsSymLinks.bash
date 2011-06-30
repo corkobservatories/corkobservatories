e13ff634537f05b3893e824c669a06c882d86f67
mheesema <mheesema@27c162d1-f8be-44a7-90e4-01648dbfe7dc>
2011-06-30 01:15:33 +0000

    Added script to set symbolic links of python scripts in Linux path

---
+++ ComputerSetup/scripts/createPythonToolsSymLinks.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+ln -s /home/corks/corkobservatory/Python-tools/calibrate/dumpBin.py /home/corks/bin/dumpBin.py
+
+ln -s /home/corks/corkobservatory/Python-tools/calibrate/calibrateLogfile.py /home/corks/bin/calibrateLogfile.py
