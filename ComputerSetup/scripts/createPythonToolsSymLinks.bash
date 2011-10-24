96f180344e961dd09eed6af52783d853977cd4bb
mheesema <mheesema@uvic.ca>
2011-10-24 23:49:36 +0000

    Made Python code install script more general

--- ComputerSetup/scripts/createPythonToolsSymLinks.bash
+++ ComputerSetup/scripts/createPythonToolsSymLinks.bash
@@ -1,4 +1,11 @@
 #!/bin/bash
-ln -s /home/corks/corkobservatory/Python-tools/calibrate/dumpBin.py /home/corks/bin/dumpBin.py
 
-ln -s /home/corks/corkobservatory/Python-tools/calibrate/calibrateLogfile.py /home/corks/bin/calibrateLogfile.py
+# Make sure users bin directory exists
+if [ ! -d FILE ]
+   then
+      mkdir ~/bin
+fi
+
+# Create links assuming that users checked out code in ~/corkobservatory
+ln -s ~/corkobservatory/Python-tools/calibrate/dumpBin.py ~/bin/dumpBin.py
+ln -s ~/corkobservatory/Python-tools/calibrate/calibrateLogfile.py ~/bin/calibrateLogfile.py
