74f78332af27d7c11c23678f7e8e89ad35b88ff8
mheesema <mheesema@27c162d1-f8be-44a7-90e4-01648dbfe7dc>
2011-11-02 04:32:17 +0000

    Added help messges and -s option to skip length test.

--- Python-tools/calibrate/calibrateLogfile.py
+++ Python-tools/calibrate/calibrateLogfile.py
@@ -11,8 +11,14 @@ def parseCMDOpts():
     # http://docs.python.org/library/optparse.html
     usage="usage: %prog [options] logfile.log"
     description="""This script find raw data in mlterm logfiles and calibrates it"""
+    help_c="""Name of calibration defined in calibrateData.py (required)"""
+    help_s="""Skip the check whether the number of calibrated value equals the number of 
+             binary input fields. Use this e.g. for 1362B where one existing channel 
+             contains garbage and is not used..."""
     parser=OptionParser(usage=usage, description=description)
-    parser.add_option("-c","--calibration",type="string",default=None)
+    parser.add_option("-c","--calibration",type="string",default=None,help=help_c)
+    parser.add_option("-s","--skip_length_check",action="store_false",dest='lengthCheck',default=False,help=help_s)
+    
     # parser.add_option("-l","--logfile",type="string",default='test.log')
     return parser.parse_args()
         
@@ -49,9 +55,12 @@ if __name__=='__main__':
             t=calibratePPCTime(int(x[0],16)).strftime('%Y-%m-%d %H:%M:%S')
             xData=[int(xi,16) for xi in x[1:]]
             Data=calibrateData(xData)
-            if len(Data)==len(xData):
+            if options.lengthCheck:
+                if len(Data)==len(xData):
+                    print t,(len(Data)*'%.4f ') % tuple(Data)
+            else:
                 print t,(len(Data)*'%.4f ') % tuple(Data)
-            #print t,(len(Data)*'%.4f ') % tuple(Data)
+                
         except:
             #print exc_info()[0]
             pass
